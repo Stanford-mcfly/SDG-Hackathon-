f7d7042095dc4ffb218f4977b7fcab159c7ea816
Lazy8055 <learnarun007@gmail.com>
2024-03-23 16:40:57 +0530

    login page added

---
+++ src/login.py
@@ -0,0 +1,73 @@
+import requests
+import streamlit as st
+
+st.set_page_config(
+    page_title="Zapspot - EV Charging station finder",
+    page_icon="img/icon.png", 
+)
+
+
+def register():
+    
+    
+    namer = st.text_input('Name ')    
+    emailr = st.text_input('Email for registration')
+    passwordr = st.text_input('Password ', type='password')
+    confirm_password = st.text_input('Confirm Password', type='password')
+    user_type = st.selectbox('User Type', ['Operator', 'User'])
+    operator = True if (user_type == 'Operator') else False
+    if st.button('Register'):
+        
+        if namer and emailr and passwordr and confirm_password:
+            if passwordr != confirm_password:
+                st.error('Passwords do not match.')
+                return
+            
+            login_data = {'name': namer, 'email': emailr, 'password': passwordr, 'is_operator' : operator}
+            response = requests.post('http://localhost:5000/register', data=login_data)
+            if(response['success']):
+                st.success('Login successful!')
+            else:
+                st.error("Wrong Email or Password")
+           
+            
+        else:
+            st.error('Please fill in all the required fields.')
+
+def login():
+    
+
+    email = st.text_input('Email')
+    password = st.text_input('Password', type='password')
+    user_type = st.selectbox('User Type', ['User', 'Operator'])
+    
+    if st.button('Login'):
+        if email and password:
+            operator = True if (user_type == 'Operator') else False
+            login_data = {'email': email, 'password': password, 'is_operator' : operator}
+            response = requests.post('http://localhost:3000/login', data=login_data)
+            if(response['success']):
+                st.success('Login successful!')
+            else:
+                st.error("Wrong Email or Password")
+
+        else:
+            st.error('Please enter your email and password.')
+   
+    
+
+def main():
+
+    st.title('Electric Vehicle Charging Navigator Registration')
+    st.write("### Have a account already?")
+    option = st.selectbox("", ["Login", "Register"])
+
+    if option == "Login":
+        login()
+    
+    elif option == "Register":
+        register()
+
+if __name__ == '__main__':
+     main()
+    
