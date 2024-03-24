c0acfa30fdea3a6f8a0c408f9a0f38eda793184e
Rishi-kumar18 <rk18102004@gmail.com>
2024-03-24 12:27:32 +0530

    final commit-Project complete
    inime sathanai than

--- src/pages/User-Profile.py
+++ src/pages/User-Profile.py
@@ -5,8 +5,9 @@ import requests
 
 def main():
         response = requests.post('http://localhost:5000/session',json={'operator': True}).json()
-        if  not (response['operator']):
+        if (response['operator']):
            st.title(f"Welcome {response['name']}")
+           st.write("## EV Details:")
            ev = st.text_input("Enter your EV Model:")
            charge = st.radio("Select the charger type:", ("Type 1", "Type 2","Type 3"))
            slot = st.number_input("Enter your phone no:",format='%.0f')
@@ -18,5 +19,26 @@ def main():
               
         else:
             st.error("You are not a User")
+
+        st.write('---')
+        st.write("## Emergency Alert:")
+        st.write("Send Emergency Alert to the nearby EV Charging Station to request for help")
+        st.write("##")
+
+        if st.button("Emergency"):
+          st.success("Emergency Alert Sended to the nearby EV Station")
+
+        st.write('---')
+        st.write("## Send Feedback about the App:")
+        st.text_input("Enter Email id:")
+        st.text_area("Feedback about our App:")
+        if(st.button("Send")):
+          st.success("Feedback sent!")
+                      
+
+
+
+
+
 if __name__ == "__main__":
     main()
